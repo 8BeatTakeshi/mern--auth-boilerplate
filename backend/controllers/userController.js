17e6ecdcfdfd9d457a2ff7a6d7be6032d74e6ecb
8BeatTakeshi <d.nakamura75@gmail.com>
2023-11-20 23:34:56 +0100

    hash paswword and create user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,7 @@
 import asyncHandler from "express-async-handler";
 
+import User from "../models/userModel.js";
+
 // @desc Auth user/set token
 // route POST /api/users/auth
 // @access Public
@@ -11,6 +13,24 @@ const authUser = asyncHandler(async (req, res) => {
 // route POST /api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exist");
+  }
+
+  const user = await User.create({ name, email, password });
+
+  if (user) {
+    res.status(201).json({ _id: user._id, name: user.name, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+
   res.status(200).json({ message: "Register User" });
 });
 
