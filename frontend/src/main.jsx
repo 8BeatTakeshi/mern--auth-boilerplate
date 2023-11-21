ae2e8e71a9e026ee1f43eeecfb1fab9ba59ee619
8BeatTakeshi <d.nakamura75@gmail.com>
2023-11-21 17:46:26 +0100

    set redux

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,9 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import { Provider } from "react-redux";
 
+import store from "./redux/store.js";
 import App from "./App.jsx";
 import HomeScreen from "./screens/HomeScreen.jsx";
 import LoginScreen from "./screens/LoginScreen.jsx";
@@ -25,7 +27,9 @@ const router = createBrowserRouter(
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 );
