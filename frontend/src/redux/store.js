6f8dc7a4ff2447d14bc4dbbd032961d97fd6b1d3
8BeatTakeshi <d.nakamura75@gmail.com>
2023-11-24 18:19:31 +0100

    add auth slice

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import authReducer from "./slices/authSlices";
+
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducer,
+  },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
   devTools: true,
 });
