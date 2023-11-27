ab7a696a798e8fdcd8ce8283d518f4f707bd048a
8BeatTakeshi <d.nakamura75@gmail.com>
2023-11-27 17:28:30 +0100

    implement register functionality in front end

--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -1,8 +1,13 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { Form, Button, Row, Col } from "react-bootstrap";
+import { toast } from "react-toastify";
 
 import FormContainer from "../components/FormContainer";
+import Loader from "../components/Loader";
+import { useRegisterMutation } from "../redux/slices/usersApiSlice";
+import { setCredentials } from "../redux/slices/authSlices";
 
 const RegisterScreen = () => {
   const [name, setName] = useState("");
@@ -10,8 +15,31 @@ const RegisterScreen = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { userInfo } = useSelector((state) => state.auth);
+  const [register, { isLoading, error }] = useRegisterMutation();
+
+  useEffect(() => {
+    if (userInfo) navigate("/");
+  }, [navigate, userInfo]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match");
+    } else {
+      try {
+        const response = await register({ name, email, password }).unwrap();
+
+        dispatch(setCredentials({ ...response }));
+        navigate("/");
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
+      }
+    }
 
     console.log("Submit");
   };
@@ -59,6 +87,8 @@ const RegisterScreen = () => {
             onChange={(e) => setConfirmPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
+
+        {isLoading && <Loader />}
 
         <Button type="submit" variant="primary" className="mt-3">
           Sign Up
